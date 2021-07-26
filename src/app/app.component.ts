2e5134aa777e571488c189c532f305807411c306
Adarsh Thakur <adarsh.thakur@deskera.com>
2021-07-26 21:21:59 +0530

    what is module and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // it tells how this component should be used in HTML -> <app-root></app-root>
+  templateUrl: './app.component.html', // the view or html of the component
+  styleUrls: ['./app.component.css'] // styles that can will  be applied to component
 })
 export class AppComponent {
-  title = 'angular-training-app';
+  title = 'This is a Angular Component';
+  buttonName = 'Click me';
+
+  onButtonClick() {
+    alert("Thanks for clicking the button");
+  }
 }
+
+// Angular Divides Component in 3 sub-part
+// view -> .html -> HTML(template)
+// style -> actual styles of component -> CSS
+// functional code -> component.ts -> JS(TS)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,59 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule(
+  { // this is a NgModule Decorator -> @decoratorName -> it is way of telling angular what type of data we are providing
+    declarations: [
+      AppComponent,
+    ],
+    imports: [
+      BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+  }
+)
+
+
+  /*
+  @decoratorName(object) -> we have to feed it a object -> object will have metadata about your module
+  object -> {
+    declaration : [] -> // all the component,services under this module must be declare here.
+    imports: [] ->// all the dependent module that has been used in this module(subsequent component)
+    providers: [] -> // object of the service that need to be injected
+    bootstrap:[] -> // the components to load when this module loads
+  }
+  */
 export class AppModule { }
+
+
+
+
+// -------------------------- TS ----------------------------
+
+// Typescript - Language Build upon JS -> Superset of javascript
+// TS -> Transpile -> JS
+// Providing a static type checking to JS
+/*
+JS
+let foo;
+foo = 10;
+OR
+foo = "This if Fu";
+OR
+foo = true;
+
+function x(){
+  foo * 2;
+}
+
+
+TS
+let foo:number;
+
+foo = 10;
+// after 1000 lines of code
+
+foo = "This is a Fu";  -> this will give a type error
+
+*/
